c5d3aeb54cd9c7aae754bcd53082be5fe3df1aeb
olamide jubril <olamidejubril68@gmail.com>
2023-05-20 15:09:33 +0100

    Make it better

--- src/config/AppConfig.ts
+++ src/config/AppConfig.ts
@@ -1,7 +1,4 @@
-import { env } from "./EnvironmentConfig";
-
 export const APP_NAME = 'Protein Analyzer';
-export const API_BASE_URL = env?.API_ENDPOINT_URL as string;
 export const APP_PREFIX_PATH = '/app';
 export const AUTH_PREFIX_PATH = '/auth';
 export const REDIRECT_URL_KEY = 'redirect'
